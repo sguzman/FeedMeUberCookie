c7cf25d74a9ddaac02a323b6242cd29cfd3bed10
Salvador Guzman <guzmansalv@gmail.com>
2017-12-10 20:50:02 -0800

    put actions into method calls

--- src/main/scala/com/github/sguzman/scala/uber/data/Main.scala
+++ src/main/scala/com/github/sguzman/scala/uber/data/Main.scala
@@ -2,12 +2,12 @@ package com.github.sguzman.scala.uber.data
 
 import com.github.sguzman.scala.uber.data.typesafe.data.all_data.AllDataStatement
 import com.github.sguzman.scala.uber.data.typesafe.verify.PlatformChromeNavData
+import io.circe.generic.auto._
+import io.circe.parser.decode
 import org.feijoas.mango.common.base.Preconditions
 
 import scala.util.{Failure, Success}
 import scalaj.http.Http
-import io.circe.parser.decode
-import io.circe.generic.auto._
 
 object Main {
   def main(args: Array[String]): Unit = {
@@ -17,28 +17,35 @@ object Main {
       val cookies = System.getenv("COOKIES")
       Preconditions.checkNotNull(cookies)
 
-      val url = "https://partners.uber.com/p3/platform_chrome_nav_data"
-      val checkRequest = Http(url).header("Cookie", cookies)
-      val checkResponse = checkRequest.asString
+      assertCookie(cookies)
+      val allData = getAllData(cookies)
+      println(allData)
 
-      val checkBody = checkResponse.body
-      println(checkBody)
-
-      val checkObj = decode[PlatformChromeNavData](checkBody)
-      Preconditions.checkArgument(checkObj.isRight)
-      println(checkObj.right.get)
-
-      val allDataUrl = "https://partners.uber.com/p3/money/statements/all_data/"
-      val allDataRequest = Http(allDataUrl).header("Cookie", cookies)
-      val allDataResponse = allDataRequest.asString
-
-      val allDataBody = allDataResponse.body
-      val allDataObj = decode[Array[AllDataStatement]](allDataBody)
-      Preconditions.checkArgument(allDataObj.isRight)
-      println(allDataObj.right.get)
     }) match {
       case Success(_) => println("Done")
       case Failure(e) => Console.err.println(e)
     }
+  }
+
+  def assertCookie(cookies: String): Unit = {
+    val url = "https://partners.uber.com/p3/platform_chrome_nav_data"
+    val checkRequest = Http(url).header("Cookie", cookies)
+    val checkResponse = checkRequest.asString
+
+    val checkBody = checkResponse.body
+    val checkObj = decode[PlatformChromeNavData](checkBody)
+    Preconditions.checkArgument(checkObj.isRight)
+  }
+
+  def getAllData(cookies: String): Array[AllDataStatement] = {
+    val allDataUrl = "https://partners.uber.com/p3/money/statements/all_data/"
+    val allDataRequest = Http(allDataUrl).header("Cookie", cookies)
+    val allDataResponse = allDataRequest.asString
+
+    val allDataBody = allDataResponse.body
+    val allDataObj = decode[Array[AllDataStatement]](allDataBody)
+    Preconditions.checkArgument(allDataObj.isRight)
+
+    allDataObj.right.get
   }
 }
