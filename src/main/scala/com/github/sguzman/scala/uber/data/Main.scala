d199ef5b8f1f44a39544f31bf3f513ac41a22eea
Salvador Guzman <guzmansalv@gmail.com>
2017-12-10 20:24:12 -0800

    assert return type of request

--- src/main/scala/com/github/sguzman/scala/uber/data/Main.scala
+++ src/main/scala/com/github/sguzman/scala/uber/data/Main.scala
@@ -1,9 +1,13 @@
 package com.github.sguzman.scala.uber.data
 
+import com.github.sguzman.scala.uber.data.typesafe.verify.PlatformChromeNavData
 import org.feijoas.mango.common.base.Preconditions
 
 import scala.util.{Failure, Success}
 import scalaj.http.Http
+
+import io.circe.parser.decode
+import io.circe.generic.auto._
 
 object Main {
   def main(args: Array[String]): Unit = {
@@ -14,7 +18,11 @@ object Main {
       Preconditions.checkNotNull(cookies)
 
       val url = "https://partners.uber.com/p3/platform_chrome_nav_data"
-      println(Http(url).header("Cookie", cookies).asString)
+      val checkRequest = Http(url).header("Cookie", cookies)
+      val checkResponse = checkRequest.asString
+      val checkBody = checkResponse.body
+      val checkObj = decode[PlatformChromeNavData](checkBody)
+      println(checkObj)
     }) match {
       case Success(_) => println("Done")
       case Failure(e) => Console.err.println(e)
