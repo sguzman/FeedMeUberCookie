5f0f3bb3b35838207e7d35e63d717f4dbcb0809b
Salvador Guzman <guzmansalv@gmail.com>
2017-12-10 19:49:28 -0800

    ignore login features for now

--- src/main/scala/com/github/sguzman/scala/uber/data/Main.scala
+++ src/main/scala/com/github/sguzman/scala/uber/data/Main.scala
@@ -1,11 +1,20 @@
 package com.github.sguzman.scala.uber.data
 
+import org.feijoas.mango.common.base.Preconditions
+
 import scala.util.{Failure, Success}
+import scalaj.http.Http
 
 object Main {
   def main(args: Array[String]): Unit = {
     util.Try({
-      val response = Login.apply
+      //val response = Login.apply
+
+      val cookies = System.getenv("COOKIES")
+      Preconditions.checkNotNull(cookies)
+
+      val url = "https://partners.uber.com/p3/platform_chrome_nav_data"
+      println(Http(url).header("Cookie", cookies).asString)
     }) match {
       case Success(_) => println("Done")
       case Failure(e) => Console.err.println(e)
